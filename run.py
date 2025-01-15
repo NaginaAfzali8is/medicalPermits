8c82619a3761623eb16fbe41322f3d29db79644d
Nagina <naginamafzal@i8is.com>
2025-01-16 01:20:56 +0500

    printt

--- run.py
+++ run.py
@@ -193,7 +193,7 @@ def check_passport_existenceOld():
     
     except Exception as e:
         # Handle any database or application errors
-        print("error": str(e))
+        print("error", str(e))
         return jsonify({"error": str(e)}), 500
 
 
